201d02b023d3b56c2e37871b6c6a432114d2bd46
mederit <iskandar.u.meder@gmail.com>
2021-03-24 19:29:00 +0600

    Static files don't work

--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class SizeVariant(models.Model):
+    size = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.size
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    price = models.CharField(max_length=20)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    size = models.ForeignKey(SizeVariant, on_delete=models.PROTECT, null=True, blank=True)
+
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.product_name
+
+
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        if self.image:
+            return self.image.url
+        return ''
