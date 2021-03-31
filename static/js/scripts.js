5d28c1ce9429990cc4e2d84e14ced20ea0180282
mederit <iskandar.u.meder@gmail.com>
2021-03-31 23:30:08 +0600

    modal window

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -54,3 +54,20 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+
+
+// модальное окно
+
+
+$('#productModal').on('show.bs.modal', function (event) {
+  let button = $(event.relatedTarget);
+  let url = button.data('url');
+  let container = $(this).find('.modal-product');
+  container.html('');
+  $.ajax({
+    url: url,
+  }).done(function(data){
+    container.html(data);
+  });
+});
